b8883a04299037a1ee5f376a088316d98dd5da7a
satthuno999 <binh9aqktk@gmail.com>
2022-12-28 03:27:45 +0700

    init

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\MusicNC\Service;
+
+class NoteNotFound extends \Exception {
+}
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+module.exports = webpackConfig
