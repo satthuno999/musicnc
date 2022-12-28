7d0c876b81e43427a22ea341a0f0b722909c93cf
satthuno999 <binh9aqktk@gmail.com>
2022-12-29 03:23:29 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
 import { generateFilePath } from '@nextcloud/router'
 
 import Vue from 'vue'
-import App from './App'
+import App from './musicnc'
 
 // eslint-disable-next-line
 __webpack_public_path__ = generateFilePath(appName, '', 'js/')
