bbf53eca89c5231608c16c48908346586e662151
satthuno999 <binh9aqktk@gmail.com>
2022-12-29 03:32:55 +0700

    a

--- templates/main.php
+++ templates/main.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 // SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 ?>
-<div id="content">123</div>
+<!-- <div id="content">123</div> -->
