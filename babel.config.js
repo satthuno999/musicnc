de1e6617360bd62b28385468f654b5b45425e1fe
satthuno999 <binh9aqktk@gmail.com>
2023-01-17 12:01:44 +0700

    init VUE NOTE
    start

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
-<?php
-declare(strict_types=1);
 // SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
-?>
-<div id="content"></div>
+const babelConfig = require('@nextcloud/babel-config')
+
+module.exports = babelConfig
--- lib/Service/NoteNotFound.php
+++ lib/Service/NoteNotFound.php
@@ -2,5 +2,8 @@
 declare(strict_types=1);
 // SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
-?>
-<div id="content"></div>
+
+namespace OCA\MusicNC\Service;
+
+class NoteNotFound extends \Exception {
+}
