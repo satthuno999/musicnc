13fefa7208e0b76907b4f49f1b20c8dce1083df3
satthuno999 <binh9aqktk@gmail.com>
2023-01-17 14:23:14 +0700

    CLEFT

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import CLeft from "./components/CLeft.vue"
+</script>
 <template>
     <!--
     SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
@@ -6,17 +9,16 @@
 	<div id="content" class="app-musicnc bg-[#e9e9e9] dark:bg-[#888] px-[12%] py-5 min-h-screen grid justify-center items-center">
 			<div
 				class="main-conntainer border-1 border-solid border-[#d9d9d9] shadow-lg shadow-gray-300 dark:shadow-gray-600 dark:border-none grid grid-cols-4 rounded-3xl">
-				<!-- <CLeft class="col-span-1 bg-white" /> -->
+				<CLeft class="col-span-1 bg-white" />
 				<!-- <RouterView class="col-span-3 bg-[#E7E8F3]" /> -->
 			</div>
 			<div class="text-center text-xs text-gray-400 dark:text-gray-600">
 				<p>
 					Copyright 2022-PRESENT <sup>&copy;</sup>
-					<a href="https://github.com/thuongtruong1009/bee-music">thuongtruong1009</a>.
-					<a href="https://github.com/thuongtruong1009/bee-music/blob/main/LICENSE">All rights reserved</a>!
+					<a href="https://github.com/satthuno999/musicnc">satthuno99</a>.
+					<a href="https://github.com/satthuno999/musicnc/LICENSE">All rights reserved</a>!
 				</p>
 			</div>
-		<!-- <AppNavigation>
 			<AppNavigationNew v-if="!loading"
 				:text="t('musicnc', 'New note')"
 				:disabled="false"
