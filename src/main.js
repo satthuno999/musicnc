95bb3a788a98888ea3012b251c3f2586d1e6ab75
satthuno999 <binh9aqktk@gmail.com>
2023-01-17 12:37:50 +0700

    init index

--- src/main.js
+++ src/main.js
@@ -14,6 +14,6 @@ __webpack_public_path__ = generateFilePath(appName, '', 'js/')
 Vue.mixin({ methods: { t, n } })
 
 export default new Vue({
-	el: '#content',
+	el: '#app',
 	render: h => h(App),
 })
