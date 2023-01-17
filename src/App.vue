c120be16f5bacb0174639df7d31872d598379b6e
satthuno999 <binh9aqktk@gmail.com>
2023-01-17 13:51:28 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import CLeft from "./components/CLeft.vue"
-</script>
+</script> -->
 <template>
 	<!--
     SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
@@ -10,7 +10,7 @@ import CLeft from "./components/CLeft.vue"
 		<div class="bg-[#e9e9e9] dark:bg-[#888] px-[12%] py-5 min-h-screen grid justify-center items-center">
 			<div
 				class="main-conntainer border-1 border-solid border-[#d9d9d9] shadow-lg shadow-gray-300 dark:shadow-gray-600 dark:border-none grid grid-cols-4 rounded-3xl">
-				<CLeft class="col-span-1 bg-white" />
+				<!-- <CLeft class="col-span-1 bg-white" /> -->
 				<!-- <RouterView class="col-span-3 bg-[#E7E8F3]" /> -->
 			</div>
 			<div class="text-center text-xs text-gray-400 dark:text-gray-600">
@@ -24,7 +24,7 @@ import CLeft from "./components/CLeft.vue"
 	</div>
 </template>
 
-<style>
+<!-- <style>
 @import "./assets/base.css";
 @import "./assets/_variables.css";
-</style>
+</style> -->
