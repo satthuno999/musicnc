8c46f68708f37438b7cbf510ff1210576159de9f
satthuno999 <binh9aqktk@gmail.com>
2023-02-14 10:29:37 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 module.exports = {
-	extends: [
-		'@nextcloud',
-	]
+	extends: ['@nextcloud'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
 }
