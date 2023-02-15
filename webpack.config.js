355ca86f07e45ca5ccce0b713f7b0bb25dcc2b13
satthuno999 <binh9aqktk@gmail.com>
2023-02-16 00:51:44 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 // SPDX-FileCopyrightText: Vũ Xuân Bình <binh9aqktk@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const { VueLoaderPlugin } = require("vue-loader");
 
-module.exports = webpackConfig
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [
+    // make sure to include the plugin!
+    webpackConfig,
+    new VueLoaderPlugin(),
+  ],
+};
