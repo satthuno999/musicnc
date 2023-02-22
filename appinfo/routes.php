f91e84e732be0f5749e72189549323ac5a071e6d
satthuno999 <binh9aqktk@gmail.com>
2023-02-22 23:18:34 +0700

    Update routes.php

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -17,7 +17,7 @@ return [
 		'note_api' => ['url' => '/api/0.1/notes']
 	],
 	'routes' => [
-		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+		['name' => 'main#index', 'url' => '/', 'verb' => 'GET'],
 		['name' => 'note_api#preflighted_cors', 'url' => '/api/0.1/{path}',
 			'verb' => 'OPTIONS', 'requirements' => ['path' => '.+']]
 	]
