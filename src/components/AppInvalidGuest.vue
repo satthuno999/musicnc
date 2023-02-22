608eaeebdb257aa31a1b2ae369742134b450aa48
satthuno999 <binh9aqktk@gmail.com>
2023-02-22 14:23:01 +0700

    Create AppInvalidGuest.vue

---
+++ src/components/AppInvalidGuest.vue
@@ -0,0 +1,85 @@
+<template>
+    <NcContent app-name="musicnc">
+        <NcAppContent>
+            <div class="main">
+                <div class="dialog">
+                    <div class="message">
+                        {{ t("musicnc", "Cannot access recipe folder.") }}
+                    </div>
+                    <div>
+                        {{
+                            // prettier-ignore
+                            t("musicnc", "You are logged in with a guest account. Therefore, you are not allowed to generate arbitrary files and folders on this Nextcloud instance. To be able to use the musicnc app as a guest, you need to specify a folder where all recipes are stored. You will need write permission to this folder."                            )
+                        }}
+                    </div>
+                    <div>
+                        <button @click.prevent="selectFolder">
+                            {{ t("musicnc", "Select recipe folder") }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </NcAppContent>
+    </NcContent>
+</template>
+
+<script>
+import NcContent from "@nextcloud/vue/dist/Components/NcContent"
+import NcAppContent from "@nextcloud/vue/dist/Components/NcAppContent"
+
+export default {
+    name: "InvalidGuest",
+    components: {
+        // eslint-disable-next-line vue/no-reserved-component-names
+        NcContent,
+        NcAppContent,
+    },
+    methods: {
+        selectFolder() {
+            OC.dialogs.filepicker(
+                t("musicnc", "Path to your recipe collection"),
+                (path) => {
+                    this.$store
+                        .dispatch("updateRecipeDirectory", { dir: path })
+                        .then(() => {
+                            window.location.reload()
+                        })
+                },
+                false, // Single result
+                ["httpd/unix-directory"], // Desired MIME type
+                true // Make modal dialog
+            )
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .dialog {
+        width: 50%;
+        padding: 20px;
+        border: 1px solid;
+        border-radius: 15px;
+
+        @media screen and (max-width: 800px) {
+            width: 90%;
+        }
+
+        div {
+            margin: 20px 5px;
+        }
+
+        .message {
+            font-size: x-large;
+            font-weight: bold;
+        }
+    }
+}
+</style>
