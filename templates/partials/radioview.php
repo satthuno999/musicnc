ec38c36fa92665d804163364589b489449f9d610
satthuno999 <41618648+satthuno999@users.noreply.github.com>
2023-05-19 09:27:16 +0700

    error router

--- templates/partials/radioview.php
+++ templates/partials/radioview.php
@@ -1,6 +1,14 @@
 <?php
-    $responseData = json_decode($data, true);
+/**
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the LICENSE.md file.
+ *
+ * @author S P A R K <binh9aqktk@gmail.com>
+ * @copyright 2022-2023 S P A R K
+ */
+$responseData = json_decode($data, true);
 ?>
 <div id="radioview">
-    <?php echo "$responseData"?>
+    <?php echo "$responseData" ?>
 </div>
