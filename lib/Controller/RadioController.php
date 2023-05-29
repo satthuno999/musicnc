08436653447100ed47cf38c70dd0206d3f730ea4
satthuno999 <binh9aqktk@gmail.com>
2023-05-29 22:11:01 +0700

    click search API Zing

--- lib/Controller/RadioController.php
+++ lib/Controller/RadioController.php
@@ -23,7 +23,7 @@ class RadioController extends Controller
      */
     public function index()
     {
-        return new TemplateResponse('musicnc', 'zingview');
+        return new TemplateResponse('musicnc', 'radioview');
     }
     /**
      * @NoAdminRequired
